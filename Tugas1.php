eb149762775c2323368fc92be907331974e00032
Ridho Prabowo <48997545+dhopi@users.noreply.github.com>
2019-07-01 11:06:54 +0700

    Add files via upload

---
+++ Tugas1.php
@@ -0,0 +1,9 @@
+<?php
+  $str = "Selamat Datang";
+  echo strpos($str, "t") . "<br>";
+  echo strrev($str) . "<br>";
+  echo str_word_count($str);
+  echo strlen($str) . "<br>";
+  echo strtolower($str) . "<br>";
+  
+ ?>
---
+++ Tugas4.php
@@ -0,0 +1,8 @@
+<?php
+   $names = array("Andi", "Boni", "Tayo", "Willy", "sunny");
+   $x = 4;
+   for($x=0;$x<count($names);$x++){
+	   echo $names[$x]."<br/>";
+   }
+
+?>
